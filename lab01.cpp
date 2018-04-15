1c2dd1e33ff3a198365b2d5f603eb33c73cfbf26
RoyYang0714 <sy2es93104@gmail.com>
2018-04-15 10:54:44 +0800

    lab01.cpp

--- lab01.cpp
+++ lab01.cpp
@@ -11,7 +11,7 @@ int flag;
 struct maze_tp
 {
 	char cont;
-	int id, pi, pj;
+	int id, pi, pj, r, d, l, u;
 };
 
 int maze_judge(int p, int n);
@@ -23,6 +23,8 @@ int main()
 	int maze_num;
 	int maze_w;
 	int maze_h;
+	char sol[10000];
+	int sol_size;
 	
 	cin>>maze_num;
 	
@@ -34,10 +36,14 @@ int main()
 		if (ans.empty()) cout<<"NO"<<endl;
 		else {
 			cout<<"YES"<<endl;
-			while (!ans.empty()) {
-				cout<<ans.top();
+			sol_size = ans.size();
+			for (int i = 0; !ans.empty(); i++) {
+				sol[i] = ans.top();
 				ans.pop();
 			}
+			for (int i = sol_size-1; i >= 0; i--) {
+				cout<<sol[i];
+			}	
 			cout<<endl;
 		}
 	}
@@ -48,6 +54,8 @@ void mazerunner(maze_tp maze[N][N], int maze_w, int maze_h)
 {
 	int i = 0;
 	int j = 0;
+	int i_tmp;
+	int j_tmp;
 	int test = 0;
 	int track = 0;
 	flag = 0;
@@ -56,8 +64,12 @@ void mazerunner(maze_tp maze[N][N], int maze_w, int maze_h)
 		for (int j = 0; j < maze_w; j++) {
 			cin>>maze[i][j].cont;
 			maze[i][j].id = 0;
-			maze[i][j].pi = N;
-			maze[i][j].pj = N;
+			maze[i][j].pi = 0;
+			maze[i][j].pj = 0;
+			maze[i][j].d = 0;
+			maze[i][j].u = 0;
+			maze[i][j].l = 0;
+			maze[i][j].r = 0;
 			cout<<maze[i][j].cont<<' ';
 		}
 			
@@ -69,90 +81,119 @@ void mazerunner(maze_tp maze[N][N], int maze_w, int maze_h)
 		maze[0][0].id = 1;
 	}
 	while (1) {
-		usleep(500);
-		cout<<ans.top();
-		//cout<<' '<<i<<' '<<j<<' ';
+		//usleep(500);
+		//cout<<ans.top()<< ' '<<flag<<endl;
+		//cout<<' '<<i<<' '<<j<<' '<<maze[i][j].cont<<' ';
 		
-		if (maze[i+1][j].id != 1 && i+1 < maze_h && maze[i+1][j].pi != i && maze[i+1][j].pj != j) {
-			maze[i+1][j].id = 1;
-			
+		if (maze[i+1][j].id != 1 && i+1 < maze_h && maze[i][j].d != 1) {	 
+
+			maze[i][j].d = 1;
+
 			test = maze_judge (maze[i][j].cont, maze[i+1][j].cont);
 
 			if (test == 1) {
 				i++;
 				ans.push(maze[i][j].cont);
 				maze[i][j].id = 1;
+				maze[i][j].d = 0;
+				maze[i][j].u = 0;
+				maze[i][j].l = 0;
+				maze[i][j].r = 0;
 				maze[i][j].pi = i-1;
 				maze[i][j].pj = j;
 			}
 			test = 0; 
 		} 
-		else if (maze[i][j+1].id != 1 && j+1 < maze_w && maze[i][j+1].pi != i && maze[i][j+1].pj != j) {
-			maze[i][j+1].id = 1;
+		else if (maze[i][j+1].id != 1 && j+1 < maze_w && maze[i][j].r != 1) {
+
+			maze[i][j].r = 1;
 
 			test = maze_judge (maze[i][j].cont, maze[i][j+1].cont);
 
 			if (test == 1) {
 				j++;
 				ans.push(maze[i][j].cont);
-				maze[i][j].id = 1;
+				maze[i][j].d = 0;
+				maze[i][j].u = 0;
+				maze[i][j].l = 0;
+				maze[i][j].r = 0;
 				maze[i][j].pi = i;
 				maze[i][j].pj = j-1;
+				maze[i][j].id = 1;
 			}
 			test = 0;
 		} 
-		else if (maze[i][j-1].id != 1 && j > 0 && maze[i][j-1].pi != i && maze[i][j-1].pj != j) {
-			maze[i][j-1].id = 1;
+		else if (maze[i][j-1].id != 1 && j > 0 && maze[i][j].l != 1) {
+			maze[i][j].l = 1;
 			
 			test = maze_judge (maze[i][j].cont, maze[i][j-1].cont);
 
-			if (test = 1) {
+			if (test == 1) {
 				j--;
 				ans.push(maze[i][j].cont);
-				maze[i][j].id = 1;
+				maze[i][j].d = 0;
+				maze[i][j].u = 0;
+				maze[i][j].l = 0;
+				maze[i][j].r = 0;
 				maze[i][j].pi = i;
 				maze[i][j].pj = j+1;
+				maze[i][j].id = 1;
 			}
 			test = 0;
 		} 
-		else if (maze[i-1][j].id != 1 && i > 0 && maze[i-1][j].pi != i && maze[i-1][j].pj != j) {
-			maze[i-1][j].id = 1;
+		else if (maze[i-1][j].id != 1 && i > 0 && maze[i][j].u != 1) {
+			maze[i][j].u = 1;
 
 			test = maze_judge (maze[i][j].cont, maze[i-1][j].cont);
 
-			if (test = 1) {
+			if (test == 1) {
 				i--;
 				ans.push(maze[i][j].cont);
+				maze[i][j].d = 0;
+				maze[i][j].u = 0;
+				maze[i][j].l = 0;
+				maze[i][j].r = 0;
 				maze[i][j].pi = i+1;
-				maze[i][j].pj = j;				
+				maze[i][j].pj = j;	
+				maze[i][j].id = 1;			
 			} 
 			test = 0;
 		}
-		else if (i == 0 && j == 0) {
-			ans.pop();
-			i = maze[i][j].pi;
-			j = maze[i][j].pj;
-			return;
-		}
 		else {
-		//	cout<<endl;
+			if (ans.top() == ')') flag++;
+			else if (ans.top() == '(') flag--;
 			ans.pop();
 			maze[i][j].id = 0;
-			i = maze[i][j].pi;
-			j = maze[i][j].pj;
+			i_tmp = i;
+			j_tmp = j;
+			i = maze[i_tmp][j_tmp].pi;
+			j = maze[i_tmp][j_tmp].pj;
+		}
+		if (i == 0 && j == 0 && maze[0][0].r == 1 && maze[0][0].d == 1) {
+			if (ans.top() == ')') flag++;
+			else if (ans.top() == '(') flag--;
+			ans.pop();
+			maze[i][j].id = 0;
+			i_tmp = i;
+			j_tmp = j;
+			i = maze[i_tmp][j_tmp].pi;
+			j = maze[i_tmp][j_tmp].pj;
+			return;
 		}
 		if (i == maze_h-1  && j == maze_w - 1) {
 			if (flag == 0) return;
 			else {
+				if (ans.top() == ')') flag++;
+				else if (ans.top() == '(') flag--;
 				ans.pop();
 				maze[i][j].id = 0;
-				i = maze[i][j].pi;
-				j = maze[i][j].pj;
+				i_tmp = i;
+				j_tmp = j;
+				i = maze[i_tmp][j_tmp].pi;
+				j = maze[i_tmp][j_tmp].pj;
 			}
 		}
 	}
-	
-	return;
 }
 
 int maze_judge (int p, int n)
@@ -161,22 +202,28 @@ int maze_judge (int p, int n)
 	if (p == '(') {
 		if (n != ')' && n != '+' && n != '-' && n != '*' && n != '/') {
 			test = 1;
-			flag++;
+			if (n == '(') flag++;
 		}
 	}
 	else if (p == ')') {
-		if (flag != 0 && n != '(') {
+		if (n == '+' || n == '-' || n == '*' || n == '/' || n == ')') {
 			test = 1;
-			flag--;
+			if (n == ')') flag--;
 		}
 	}
 	else if (p == '+' || p == '-' || p == '*' || p == '/') {
-		if (n != '+' && n != '-' && n != '*' && n != '/' && n != ')') test = 1;
+		if (n != '+' && n != '-' && n != '*' && n != '/' && n != ')') {
+			if (n == '(') flag++;
+			test = 1;
+		}
 	}
 	else{
 		if (n == '(') test = 0;
 		else if (n == ')' && flag == 0) test = 0;
-		else test = 1;
+		else {
+			if (n == ')') flag--;
+			test = 1;
+		}
 	}
 	return test;
 }
